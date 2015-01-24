05d305678c01d40f8c3434650c9a3ae37cc515aa
Thomas Storey <storey.thomas@gmail.com>
2015-01-24 16:43:51 -0500

    tower now builds from the top down, pushing down earlier moves.

--- app/handlebars.js
+++ app/handlebars.js
@@ -3,7 +3,8 @@ _ = require('underscore')
 module.exports = function (hbs) {
 	hbs.registerHelper('list', function (docs) {
 		var out = "";
-		_.map(docs, function(doc){
+		var sorted = _.sortBy(docs, function (doc) { return -doc.timestamp; });
+		_.map(sorted, function(doc){
 			out += "<div class='row'><div class='one-third column'><div class='timestamp'>" + doc.timestamp + "</div></div><div class='two-third column'><pre>" + doc.data + "</pre></div></div>";
 		});
 		return out;
